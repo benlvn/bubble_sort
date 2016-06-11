806f2ced2cc6613db4bff71a30017938f586b820
Bennett Levine <ben.lvn@gmail.com>
2016-06-10 21:30:58 -0400

    Added bubble_sort_by method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,3 +9,13 @@ def bubble_sort(arr)
 	return arr
 end
 
+def bubble_sort_by(arr)
+	arr.length.times do
+		arr.each_index do |ind|
+			if ind < arr.length-1 && yield(arr[ind+1], arr[ind]) < 0
+				arr[ind], arr[ind+1] = arr[ind+1], arr[ind]
+			end
+		end
+	end
+	return arr
+end
